e397cfbe224f7a572a20d7e72ed9a1c51d32278a
zahrasarabi79 <95568078+zahrasarabi79@users.noreply.github.com>
2023-01-21 12:42:29 +0330

    Add files via upload

--- clean.js
+++ clean.js
@@ -3,7 +3,7 @@ const add = require("./actions/add");
 const deleted = require("./actions/delete");
 const updatename = require("./actions/updateName");
 const updatNumber = require("./actions/updateNumber");
-async function phonebook() {
+(async()=> {
   const inquirerOptions = [
     {
       name: "phonebook",
@@ -14,18 +14,27 @@ async function phonebook() {
   ];
 
   let inquirerResult = await inquirer.prompt(inquirerOptions);
+  const total = {
+    "add number":add,
+    "updat number":updatNumber,
+    "updat name":updatename,
+    "delete number":deleted,
+  }
 
-  if (inquirerResult.phonebook == "add number") {
-     await add();
-  }
-  if (inquirerResult.phonebook == "updat number") {
-    await updatNumber();
-  }
-  if (inquirerResult.phonebook == "updat name") {
-    await updatename();
-  }
-  if (inquirerResult.phonebook == "delete number") {
-    await deleted();
-  }
-}
-phonebook();
+ await total[inquirerResult.phonebook]();
+  // total["add number"]();
+  // add();
+
+  // if (inquirerResult.phonebook == "add number") {
+  //    await add();
+  // }
+  // if (inquirerResult.phonebook == "updat number") {
+  //   await updatNumber();
+  // }
+  // if (inquirerResult.phonebook == "updat name") {
+  //   await updatename();
+  // }
+  // if (inquirerResult.phonebook == "delete number") {
+  //   await deleted();
+  // }
+})();
